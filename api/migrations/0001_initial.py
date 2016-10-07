0dc43165c5b30a3ceae1462c14b49d3b30fc3368
Andrew Madonna <andrew.w.madonna@gmail.com>
2016-10-07 00:13:03 -0400

    Add circleci (#28)
    * Add circle.yml config file
    
    * Add database config
    
    * Add postgres login permissions
    
    * Add dependant apps migrations
    
    * Try using __latest__ on dependant
    
    * Add newline

--- api/migrations/0001_initial.py
+++ api/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('genes', '0001_initial'),
+        ('genes', '__latest__'),
     ]
 
     operations = [
